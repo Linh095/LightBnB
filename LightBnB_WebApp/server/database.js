1ed2f9adb8ad3e96e33eecbc09818125c82f6e75
Linh Vu Hai Nguyen <linh.nguyen.nvhl@gmail.com>
2020-04-02 18:31:13 -0400

    added comments to functions which i'm still unsure of

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -20,7 +20,7 @@ const getUserWithEmail = function (email) {
   SELECT * FROM users
   WHERE email = $1
   `, [email])
-    .then(res => res.rows)
+    .then(res => res.rows) //need to choose first index?? res.row[0]
     .catch(_err => Promise.reject(null));
 
 }
@@ -38,7 +38,7 @@ const getUserWithId = function (id) {
   SELECT * FROM users
   WHERE id = $1
   `, [id])
-    .then(res => res.rows)
+    .then(res => res.rows) //need to choose first index?? res.row[0]
     .catch(_err => Promise.reject(null));
 }
 exports.getUserWithId = getUserWithId;
